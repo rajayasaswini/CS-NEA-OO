b27d26c56d6283674df7cc39b6c969b5917a9a31
rajayasaswini <rajayasaswini@gmail.com>
2021-11-06 17:28:27 +0400

    -

--- timeme/__init__.py
+++ timeme/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_wtf import FlaskForm
 from flask_sqlalchemy import SQLAlchemy
@@ -5,6 +6,7 @@ from datetime import datetime
 from sqlalchemy import *
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///TimeMe.db'
@@ -13,5 +15,11 @@ app.config['SECRET_KEY'] = 'cb1414668bb6f2a30c99cfb0e9c1441b'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_man = LoginManager(app)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('example@gmail.com')
+app.config['MAIL_PASSWORD'] = os.environ.get('password')
+mail = Mail(app)
 
 from timeme import routes
