90c2328477e78311ca2baac301c05464887e765f
rajayasaswini <rajayasaswini@gmail.com>
2021-11-15 18:34:54 +0400

    removed queries.py

--- timeme/queries.py
+++
@@ -1,8 +0,0 @@
-from timeme.models import *
-from wtforms_sqlalchemy.fields import QuerySelectField
-from sqlalchemy import *
-from flask_sqlalchemy import SQLAlchemy
-from timeme.models import *
-from timeme import app, db
-
-classes = db.session.query(Classes.className).filter_by(classAdminID=current_user.id).all()
